71f3c60e655a5ad20e4f0724b197e514231598dd
IgorKv <linyix7@gmail.com>
2020-09-23 11:23:23 +0300

    ver 1.0.42

--- src/main/java/com/kvelinskiy/ua/statisticsAutomation/configuration/MvcConfig.java
+++ src/main/java/com/kvelinskiy/ua/statisticsAutomation/configuration/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.kvelinskiy.ua.statisticsAutomation.configuration;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
